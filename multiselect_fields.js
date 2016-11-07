8a732aacd2c662a955347600199245f209728494
julienjoannic <julienjoannic@gmail.com>
2016-11-07 09:02:17 +0100

    Exécution différée

--- multiselect_fields.js
+++ multiselect_fields.js
@@ -1,7 +1,7 @@
-(function($){
+AJS.$(document).ready(function($){
 	new AJS.MultiSelect({
 		element: AJS.$("#customfield_11483"),
 		itemAttrDisplayed: "label",
 		errorMessage: AJS.params.multiselectComponentsError
 	});
-})(AJS.$);
+});
