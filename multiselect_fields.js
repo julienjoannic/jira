a6e32cb18a05cf72486c76534abda66e5a61467f
julienjoannic <julienjoannic@gmail.com>
2016-11-07 10:21:34 +0100

    Mode événementiel + initialisation

--- multiselect_fields.js
+++ multiselect_fields.js
@@ -1,9 +1,13 @@
 AJS.$(document).ready(function($){
 	JIRA.bind(JIRA.Events.NEW_CONTENT_ADDED, function (e,context) {
+		configureMultiSelect();
+    });
+	
+	function configureMultiSelect() {
 		new AJS.MultiSelect({
 			element: AJS.$("#customfield_11483"),
 			itemAttrDisplayed: "label",
 			errorMessage: AJS.params.multiselectComponentsError
 		});
-    });
+	}
 });
