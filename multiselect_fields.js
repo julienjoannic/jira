d0bb138953b98eac220d76ec8e9275efaa0245d8
julienjoannic <julienjoannic@gmail.com>
2016-11-07 08:43:04 +0100

    javascript pour champs multiselect

---
+++ multiselect_fields.js
@@ -0,0 +1,7 @@
+(function($){
+	new AJS.MultiSelect({
+		element: AJS.$("#customfield_11483"),
+		itemAttrDisplayed: "label",
+		errorMessage: AJS.params.multiselectComponentsError
+	});
+})(AJS.$);
