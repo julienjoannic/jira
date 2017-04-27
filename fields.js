9a65f40535a929b58ddacadcab7c56d3c6900b19
julienjoannic <julienjoannic@gmail.com>
2017-04-27 10:18:20 +0200

    Configuration du select2 pour les composants SAP

--- fields.js
+++ fields.js
@@ -19,5 +19,31 @@ AJS.$(document).ready(function($){
 	
 	configureAllMultiSelects();
 	
-	AJS.$("#customfield_12080").auiSelect2();
+	AJS.$("#customfield_12080").auiSelect2({
+		placeholder: "Composant SAP",
+		minimumInputLength: 2,
+		ajax: { // instead of writing the function to execute the request we use Select2's convenient helper
+			url: "https://sn1smprd.neo.local/sap/neotech/jira/components",
+			dataType: 'json',
+			quietMillis: 250,
+			data: function (term, page) {
+				return {
+					q: term, // search term
+				};
+			},
+			results: function (data, page) { // parse the results into the format expected by Select2.
+				// since we are using custom formatting functions we do not need to alter the remote JSON data
+				return { results: data };
+			},
+			cache: true
+		},
+		initSelection: function(element, callback) {
+			
+			
+		},
+		formatResult: function(component) { return component.id + ' - ' + component.text }, 
+		formatSelection: function(component) { return component.id }, 
+		dropdownCssClass: "bigdrop", // apply css that makes the dropdown taller
+		escapeMarkup: function (m) { return m; } // we do not want to escape markup since we are displaying html in results
+	});
 });
