674b292a48b3c05ce4e471f35dc49c1c21ba968c
julienjoannic <julienjoannic@gmail.com>
2017-04-27 10:21:06 +0200

    Appel de la fonction via JIRA bind

--- fields.js
+++ fields.js
@@ -1,8 +1,4 @@
 AJS.$(document).ready(function($){
-	JIRA.bind(JIRA.Events.NEW_CONTENT_ADDED, function (e,context) {
-		configureAllMultiSelects();
-    });
-	
 	function configureMultiSelect(fieldId) {
 		new AJS.MultiSelect({
 			element: AJS.$("#" + fieldId),
@@ -17,33 +13,41 @@ AJS.$(document).ready(function($){
 		configureMultiSelect("customfield_11483");
 	}
 	
-	configureAllMultiSelects();
+	function configureFields() {
+		configureAllMultiSelects();
+		
+		AJS.$("#customfield_12080").auiSelect2({
+			placeholder: "Composant SAP",
+			minimumInputLength: 2,
+			ajax: { // instead of writing the function to execute the request we use Select2's convenient helper
+				url: "https://sn1smprd.neo.local/sap/neotech/jira/components",
+				dataType: 'json',
+				quietMillis: 250,
+				data: function (term, page) {
+					return {
+						q: term, // search term
+					};
+				},
+				results: function (data, page) { // parse the results into the format expected by Select2.
+					// since we are using custom formatting functions we do not need to alter the remote JSON data
+					return { results: data };
+				},
+				cache: true
+			},
+			initSelection: function(element, callback) {
+				
+				
+			},
+			formatResult: function(component) { return component.id + ' - ' + component.text }, 
+			formatSelection: function(component) { return component.id }, 
+			dropdownCssClass: "bigdrop", // apply css that makes the dropdown taller
+			escapeMarkup: function (m) { return m; } // we do not want to escape markup since we are displaying html in results
+		});
+	}
 	
-	AJS.$("#customfield_12080").auiSelect2({
-		placeholder: "Composant SAP",
-		minimumInputLength: 2,
-		ajax: { // instead of writing the function to execute the request we use Select2's convenient helper
-			url: "https://sn1smprd.neo.local/sap/neotech/jira/components",
-			dataType: 'json',
-			quietMillis: 250,
-			data: function (term, page) {
-				return {
-					q: term, // search term
-				};
-			},
-			results: function (data, page) { // parse the results into the format expected by Select2.
-				// since we are using custom formatting functions we do not need to alter the remote JSON data
-				return { results: data };
-			},
-			cache: true
-		},
-		initSelection: function(element, callback) {
-			
-			
-		},
-		formatResult: function(component) { return component.id + ' - ' + component.text }, 
-		formatSelection: function(component) { return component.id }, 
-		dropdownCssClass: "bigdrop", // apply css that makes the dropdown taller
-		escapeMarkup: function (m) { return m; } // we do not want to escape markup since we are displaying html in results
-	});
+	JIRA.bind(JIRA.Events.NEW_CONTENT_ADDED, function (e,context) {
+		configureFields();
+    });
+	
+	configureFields();
 });
